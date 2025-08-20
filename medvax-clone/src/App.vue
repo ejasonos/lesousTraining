68776abb6a4e0e81633522916c4bf9e546fd40a5
Ejakpevweoghene F. Onosemuode <110458887+ejasonos@users.noreply.github.com>
2025-08-20 11:13:42 +0000

    updated files

--- medvax-clone/src/App.vue
+++ medvax-clone/src/App.vue
@@ -4,7 +4,7 @@
 <template>
   <nav>
     <router-link to="/" class="px-3 py-2 rounded-md">Home</router-link>
-    <router-link to="/about" class="px-3 py-2 rounded-md text-amber-700 bg-purple-500">About</router-link>
+    <router-link to="/about" class="px-3 py-2 rounded-md">About</router-link>
   </nav>
   <router-view />
 </template>
