49195e2e8b4860f9af700df7492aaa8697e95563
Ejakpevweoghene F. Onosemuode <110458887+ejasonos@users.noreply.github.com>
2025-08-22 22:56:23 +0000

    updated files

---
+++ medvax-clone/src/components/BlogSection.vue
@@ -0,0 +1,15 @@
+<template>
+     <!-- Our Blog Section -->
+    <section class="w-full flex flex-col pt-28 xl:px-60">
+      <div class="flex flex-col gap-5 w-full justify-center items-center">
+        <h1 class="text-5xl font-bold text-black">Our Blog</h1>
+        <p class="text-xl font-light text-black w-2/3 text-center">
+          Stay Updated with Healthcare Innovations. Explore the latest in
+          healthcare technology, pharmaceutical care, and reporoductive health.
+        </p>
+      </div>
+      <div class="flex w-full">
+        <div class="flex flex-col"></div>
+      </div>
+    </section>
+</template>
--- medvax-clone/src/views/Home.vue
+++ medvax-clone/src/views/Home.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BlogSection from '../components/BlogSection.vue'
 import ConsultantSection from '../components/ConsultantSection.vue'
 import FooterSection from '../components/FooterSection.vue'
 import ImpactSection from '../components/ImpactSection.vue'
@@ -64,25 +65,15 @@ import TopnavSection from '../components/TopnavSection.vue'
     <!-- Impact Section -->
     <ImpactSection />
 
-    <!-- Our Blog Section -->
-    <section class="w-full flex flex-col pt-28 xl:px-60">
-      <div class="flex flex-col gap-5 w-full justify-center items-center">
-        <h1 class="text-5xl font-bold text-black">Our Blog</h1>
-        <p class="text-xl font-light text-black w-2/3 text-center">
-          Stay Updated with Healthcare Innovations. Explore the latest in
-          healthcare technology, pharmaceutical care, and reporoductive health.
-        </p>
-      </div>
-      <div class="flex w-full">
-        <div class="flex flex-col"></div>
-      </div>
-    </section>
-<!-- Partners section -->
- <PartnersSection />
+    <!-- Blog Section -->
+     <BlogSection />
 
-<!-- Get a consultant -->
- <ConsultantSection />
+    <!-- Partners section -->
+     <PartnersSection />
 
-<!-- Footer Section -->
- <FooterSection />
+    <!-- Get a consultant -->
+     <ConsultantSection />
+
+    <!-- Footer Section -->
+     <FooterSection />
 </template>
