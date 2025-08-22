8dd463452fa78bb124946a8f29727e445ed940e5
Ejakpevweoghene F. Onosemuode <110458887+ejasonos@users.noreply.github.com>
2025-08-22 19:51:03 +0000

    updated files

---
+++ medvax-clone/src/components/Footer.vue
@@ -0,0 +1,80 @@
+<template>
+    <!-- Footer Section -->
+    <footer class="w-full flex flex-col bg-[blue] p-2 gap-y-2">
+      <section class="flex w-full justify-between items-center p-2">
+        <img
+          src="../assets/logo-Footer.svg"
+          alt="MedVax logo"
+          class="h-14 w-auto mr-6"
+        />
+        <ul
+          class="list-none flex flex-wrap justify-between items-center w-full"
+        >
+          <li class="text-[purple] m-3 text-sm">Home</li>
+          <li class="text-white m-3 text-sm">About</li>
+          <li class="text-white m-3 text-sm">Innovation</li>
+          <li class="text-white m-3 text-sm">E-Pharmacy</li>
+          <li class="text-white m-3 text-sm">Blog</li>
+          <li class="text-white m-3 text-sm">Contact</li>
+          <li class="text-white m-3 text-sm">Privacy</li>
+          <a href="#topnav" class="decoration-none text-white m-3 text-sm">
+            Go to top
+            <span
+              class="h-10 w-10 rounded-full border flex justify-center items-center"
+              ><img
+                src="../assets/hp.svg"
+                alt="Top"
+                class="h-full w-full object-cover text-white"
+            /></span>
+          </a>
+        </ul>
+      </section>
+      <section class="rounded p-2 border-[purple] border-2 w-full flex items-center">
+        <div class="w-2/3 flex flex-wrap">
+          <button
+            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
+          >
+            <img
+              src="../assets/hp.svg"
+              alt="icon"
+              class="h-10 w-10 mx-1"
+            />info@medvaxhealth.com
+          </button>
+          <button
+            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
+          >
+            <img
+              src="../assets/hp.svg"
+              alt="icon"
+              class="h-10 w-10 mx-1"
+            />+234 912 973 8935
+          </button>
+          <button
+            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
+          >
+            <img
+              src="../assets/hp.svg"
+              alt="icon"
+              class="h-10 w-10 mx-1"
+            />FCT, Abuja, Nigeria
+          </button>
+          <button
+            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
+          >
+            <img
+              src="../assets/hp.svg"
+              alt="icon"
+              class="h-10 w-10 mx-1"
+            /><img
+              src="../assets/hp.svg"
+              alt="partners logo"
+              class="h-10 w-10 mx-1"
+            />
+          </button>
+        </div>
+        <div class="w-1/3 flex text-md text-white">
+          &copy; 2025 MedVax Health. All rights reserved
+        </div>
+      </section>
+    </footer>
+    </template>
--- medvax-clone/src/views/Home.vue
+++ medvax-clone/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
     <!-- Header Section -->
     <header
-      class="flex justify-center w-full p-4 text-white text-center text-xl bg-[#079FCE]"
+      class="flex justify-center items-center w-full p-4 text-white text-center text-xl medvax-bg-blue gap-x-1"
     >
       <img
         alt="hp icon"
-        src="../assets/hp.svg"
-        class="h=[50px] w-[50px]"
+        src="../assets/pink-icon.svg"
+        class="h-5 w-5"
       />
       Join the Movement - Be a Part of the Future of HealthCare
     </header>
-    <nav id="topnav" class="flex w-full 2xl:px-60 py-4 justify-between bg-[blue]">
+    <nav id="topnav" class="flex w-full 2xl:px-60 py-4 justify-between medvax-bg-deepblue">
       <div class="flex w-1/2 items-center">
         <img
           alt="Medvax logo"
@@ -443,82 +443,4 @@
       </button>
     </div>
 
-    <!-- Footer Section -->
-    <footer class="w-full flex flex-col bg-[blue] p-2 gap-y-2">
-      <section class="flex w-full justify-between items-center p-2">
-        <img
-          src="../assets/logo-Footer.svg"
-          alt="MedVax logo"
-          class="h-14 w-auto mr-6"
-        />
-        <ul
-          class="list-none flex flex-wrap justify-between items-center w-full"
-        >
-          <li class="text-[purple] m-3 text-sm">Home</li>
-          <li class="text-white m-3 text-sm">About</li>
-          <li class="text-white m-3 text-sm">Innovation</li>
-          <li class="text-white m-3 text-sm">E-Pharmacy</li>
-          <li class="text-white m-3 text-sm">Blog</li>
-          <li class="text-white m-3 text-sm">Contact</li>
-          <li class="text-white m-3 text-sm">Privacy</li>
-          <a href="#topnav" class="decoration-none text-white m-3 text-sm">
-            Go to top
-            <span
-              class="h-10 w-10 rounded-full border flex justify-center items-center"
-              ><img
-                src="../assets/hp.svg"
-                alt="Top"
-                class="h-full w-full object-cover text-white"
-            /></span>
-          </a>
-        </ul>
-      </section>
-      <section class="rounded p-2 border-[purple] border-2 w-full flex items-center">
-        <div class="w-2/3 flex flex-wrap">
-          <button
-            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
-          >
-            <img
-              src="../assets/hp.svg"
-              alt="icon"
-              class="h-10 w-10 mx-1"
-            />info@medvaxhealth.com
-          </button>
-          <button
-            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
-          >
-            <img
-              src="../assets/hp.svg"
-              alt="icon"
-              class="h-10 w-10 mx-1"
-            />+234 912 973 8935
-          </button>
-          <button
-            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
-          >
-            <img
-              src="../assets/hp.svg"
-              alt="icon"
-              class="h-10 w-10 mx-1"
-            />FCT, Abuja, Nigeria
-          </button>
-          <button
-            class="p-2 rounded border-[purple] border-2 text-md text-pink-600 flex items-center"
-          >
-            <img
-              src="../assets/hp.svg"
-              alt="icon"
-              class="h-10 w-10 mx-1"
-            /><img
-              src="../assets/hp.svg"
-              alt="partners logo"
-              class="h-10 w-10 mx-1"
-            />
-          </button>
-        </div>
-        <div class="w-1/3 flex text-md text-white">
-          &copy; 2025 MedVax Health. All rights reserved
-        </div>
-      </section>
-    </footer>
 </template>
