f9047a2c4497dd8e400b9129381ac63ef10df4a7
Ejakpevweoghene F. Onosemuode <110458887+ejasonos@users.noreply.github.com>
2025-08-23 03:12:04 +0000

    updated files

--- medvax-clone/src/components/BlogSection.vue
+++ medvax-clone/src/components/BlogSection.vue
@@ -1,6 +1,6 @@
 <template>
      <!-- Our Blog Section -->
-    <section class="w-full flex flex-col pt-24 medvax-text-deepblue">
+    <section class="w-full flex flex-col pt-24 p-14 medvax-text-deepblue gap-y-10">
         <!-- Title of the section -->
       <div class="flex flex-col gap-5 w-full justify-center items-center">
         <h1 class="text-5xl font-bold">Our Blog</h1>
@@ -11,7 +11,7 @@
       </div>
       <!-- List of card items -->
       <div class="flex w-full">
-        <div class="flex flex-col w-[400px] border-[skyblue] border-2 p-2.5 gap-y-2 rounded-sm medvax-bg-lightred">
+        <div class="flex flex-col w-[400px] border-[skyblue] border-2 px-3 py-6 gap-y-2 rounded-sm medvax-bg-lightblue">
           <img
           alt="safe sex"
           src="../assets/safe_sex.png"
@@ -24,7 +24,7 @@
             <p class="text-md font-normal">
                 When it comes to safe sex, condoms are just the beginning. True protection means being informed...
             </p>
-            <button class="w-full text-center p-2 rounded-sm text-white medvax-bg-blue">Read more</button>
+            <button class="w-full text-center p-1 rounded-sm text-white font-normal medvax-bg-blue">Read more</button>
           </div>
         </div>
       </div>
--- medvax-clone/src/components/PartnersSection.vue
+++ medvax-clone/src/components/PartnersSection.vue
@@ -1,7 +1,7 @@
 <template>
     
     <!-- Our Partners Section -->
-    <section class="w-full flex flex-col bg-white pt-28 xl:px-60">
+    <section class="w-full flex flex-col bg-white pt-28">
       <div class="flex flex-col gap-5 w-full justify-center items-center">
         <h1 class="text-5xl font-bold text-black">Our Partners</h1>
         <p class="text-xl font-light text-black w-2/3 text-center">MedVax Health, collaborates with global and local health organizations, pharmacies, and logistics partners to strengthen healthcare access and delivery.</p>
